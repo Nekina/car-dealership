a62e68888bad41164a7f2b1652cccbd26393ba46
Nekina <joshuaacusta@gmail.com>
2025-08-05 01:57:42 +0800

    Last linting fix

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,22 +32,26 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     (
-        'https://joshuaacusta-8000.theianext-1-labs-prod-misc-tools-us-east-0'
+        'https://joshuaacusta-8000'
+        '.theianext-1-labs-prod-misc-tools-us-east-0'
         '.proxy.cognitiveclass.ai'
     ),
     (
-        'https://joshuaacusta-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01'
+        'https://joshuaacusta-8000'
+        '.theiadockernext-0-labs-prod-theiak8s-4-tor01'
         '.proxy.cognitiveclass.ai'
     ),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     (
-        'https://joshuaacusta-8000.theianext-1-labs-prod-misc-tools-us-east-0'
+        'https://joshuaacusta-8000'
+        '.theianext-1-labs-prod-misc-tools-us-east-0'
         '.proxy.cognitiveclass.ai'
     ),
     (
-        'https://joshuaacusta-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01'
+        'https://joshuaacusta-8000'
+        '.theiadockernext-0-labs-prod-theiak8s-4-tor01'
         '.proxy.cognitiveclass.ai'
     ),
 ]
